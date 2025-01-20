b55a86575df7d98be36fe4fefd5094c42803737b
Eduardo Augusto Pires Hermanson <eduardowhermanson@gmail.com>
2025-01-20 16:09:31 -0300

    confirmado

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -71,7 +71,7 @@ const Projects = () => {
                   <Button variant="outline" size="sm" className="border-2 hover:bg-gray-50 transition-all duration-300" asChild>
                     <a href={project.github} target="_blank" rel="noopener noreferrer">
                       <Github className="w-4 h-4 mr-2" />
-                      GitHub
+                      GitHub-Link
                     </a>
                   </Button>
                   <Button variant="outline" size="sm" className="border-2 hover:bg-gray-50 transition-all duration-300" asChild>
